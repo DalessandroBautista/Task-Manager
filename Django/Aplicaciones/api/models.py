3b77b5ec4e5b01c75e9dea3682b237c50d70ccc4
DalessandroBautista <dalessandrobau@gmail.com>
2021-11-10 20:31:23 -0300

    Creacion del objeto Folder, y la vista FolderView

--- Django/Aplicaciones/api/models.py
+++ Django/Aplicaciones/api/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Folder(models.Model):
+    name = models.CharField(max_length=100)
+    date=models.DateTimeField
 class Task(models.Model):
     name = models.CharField(max_length=100)
     date=models.DateTimeField
     state=models.BooleanField(default='False')
+    folder = models.ForeignKey(Folder, on_delete=models.CASCADE, default=0)
+
--- Django/Aplicaciones/api/urls.py
+++ Django/Aplicaciones/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import TaskView
 urlpatterns = [
-    path('tasks/', TaskView.as_view(), name="task_list")
+    path('tasks/', TaskView.as_view(), name="tasks_list"),
+    path('folders/', TaskView.as_view(), name="folders_list"),
+    path('folders/<int:id>', TaskView.as_view(), name="folder")
 ]
