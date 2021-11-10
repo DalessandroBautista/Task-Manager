a6c62e8e9fa68e0ef7b1d69bc09eef7fb97c7241
DalessandroBautista <dalessandrobau@gmail.com>
2021-11-10 20:12:44 -0300

    Agrego creacion del proyecto en django, y el modelado de la clase Task, la vista TaskView, con el método GET

---
+++ Django/Aplicaciones/api/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from .views import TaskView
+urlpatterns = [
+    path('tasks/', TaskView.as_view(), name="task_list")
+]
---
+++ Django/Aplicaciones/api/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.views import View
+from .models import Task
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+import json
+# Create your views here.
+class TaskView(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    def get(self, request):
+        tasks=list(Task.objects.values())
+        if len(tasks)>0:
+            datos={'message':"Success", 'tasks':tasks}
+        else: datos={'message':"Task not found"}
+        return JsonResponse(datos)
+    def post(self, request):
+        jd=json.loads(request.body)
+        Task.objects.create(name=jd['name'])
+        datos={'message':"Success"}
+        return JsonResponse(datos)
+
+    def put(self, request):
+        pass
+
+    def delete(self, request):
+        pass
